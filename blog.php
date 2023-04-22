1dac8be5a5a8f5fb6e873d3e0324104aeb4b321a
Ronaldo Awademe <92430393+Ronaldo-F-dev@users.noreply.github.com>
2023-04-22 03:18:17 +0100

    Add files via upload

---
+++ blog.php
@@ -0,0 +1,18 @@
+<!Doctype html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <nav>
+            <ul>
+                <li><a href="index">Acceuil</a></li>
+                <li><a href="blog">Blog</a></li>
+                <li><a href="contact.html">Contact</a></li>
+            </ul>
+        </nav>
+        <h1>
+            Blog <?=$_GET['id'];?>
+        </h1>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!Doctype html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <nav>
+            <ul>
+                <li><a href="index.php">Acceuil</a></li>
+                <li><a href="blog.php?id=8">Blog</a></li>
+                <li><a href="contact.html">Contact</a></li>
+            </ul>
+        </nav>
+        <h1>Acceuil</h1>
+    </body>
+</html>
